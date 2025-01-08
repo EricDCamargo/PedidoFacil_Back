e38a5211a8385482f2e2b89443ff0b9c1ed5a246
EricDCamargo <ericdcamargo@gmail.com>
2025-01-08 17:53:49 -0300

    first commit

---
+++ src/controllers/product/CreateProductController.ts
@@ -0,0 +1,28 @@
+import { Response, Request } from 'express'
+import { CreateProductService } from '../../services/product/CreateProductService'
+
+class CreateProductController {
+  async handle(req: Request, res: Response) {
+    const { name, price, description, banner, category_id } = req.body
+
+    const createProductService = new CreateProductService()
+
+    if (!req.file) {
+      throw new Error('error uploading product file')
+    } else {
+      const { originalname, filename: banner } = req.file
+
+      const product = await createProductService.execute({
+        name,
+        price,
+        description,
+        banner,
+        category_id
+      })
+
+      return res.json(product)
+    }
+  }
+}
+
+export { CreateProductController }
