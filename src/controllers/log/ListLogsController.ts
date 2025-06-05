b5bd80141f276e1a2849d9819bdfe0979030ff58
EricDCamargo <ericdcamargo@gamil.com>
2025-06-04 21:44:09 -0300

    feat: enhance log retrieval by adding user_id and date filters

--- src/controllers/log/ListLogsController.ts
+++ src/controllers/log/ListLogsController.ts
@@ -5,10 +5,18 @@ import { AppError } from '../../errors/AppError'
 
 class ListLogsController {
   async handle(req: Request, res: Response) {
+    const user_id = req.query.user_id as string
+    const startDate = req.query.startDate as string
+    const endDate = req.query.endDate as string
+
     const listLogsService = new ListLogsService()
 
     try {
-      const logs = await listLogsService.execute()
+      const logs = await listLogsService.execute({
+        user_id: user_id,
+        startDate: startDate ? new Date(startDate) : undefined,
+        endDate: endDate ? new Date(endDate) : undefined
+      })
 
       return res.status(StatusCodes.OK).json(logs)
     } catch (error) {
--- src/services/log/ListLogsService.ts
+++ src/services/log/ListLogsService.ts
@@ -1,9 +1,36 @@
 import { AppResponse } from '../../@types/app.types'
 import prismaClient from '../../prisma'
 
+interface ListLogsParams {
+  user_id?: string
+  startDate?: Date
+  endDate?: Date
+}
+
 class ListLogsService {
-  async execute(): Promise<AppResponse> {
+  async execute({
+    user_id,
+    startDate,
+    endDate
+  }: ListLogsParams): Promise<AppResponse> {
+    const where: any = {}
+
+    if (user_id) {
+      where.user_id = user_id
+    }
+
+    if (startDate || endDate) {
+      where.created_at = {}
+      if (startDate) {
+        where.created_at.gte = startDate
+      }
+      if (endDate) {
+        where.created_at.lte = endDate
+      }
+    }
+
     const logs = await prismaClient.log.findMany({
+      where,
       include: {
         user: {
           select: {
